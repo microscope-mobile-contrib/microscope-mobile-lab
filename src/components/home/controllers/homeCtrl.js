c62aa241cfe3124706028d0a240263786a67d913
Thomas Adjadj <ta@ecofip.com>
2015-06-24 14:40:26 +0200

    add select device

--- src/components/home/controllers/homeCtrl.js
+++ src/components/home/controllers/homeCtrl.js
@@ -3,15 +3,33 @@
 var _$sce;
 
 // HomeCtrl class
-function HomeCtrl ($sce, $scope) {
+function HomeCtrl($sce, $scope) {
 	_$sce = $sce;
-	
+
 	this.source = '';
 	this.iframe = {
 		url: 'http://codepen.io/tomadj/full/ZGaKor/'
 	};
-	
+
+
 	this.getSource();
+
+	this.devices = [
+		{
+			name: 'iphone',
+			deviceClass: 'device-iphone',
+			iframeClass: 'iframe-iphone',
+			img: 'iphone.png'
+		},
+		{
+			name: 'ipad',
+			deviceClass: 'device-ipad',
+			iframeClass: 'iframe-ipad',
+			img: 'ipad.png'
+		}];
+
+	this.selectedDevice = this.devices[0];
+
 }
 
 HomeCtrl.prototype.getSource = function () {
