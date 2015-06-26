0a6abcef1b1e606a9ea39d6471094b59bd2b0ef9
Thomas Adjadj <ta@ecofip.com>
2015-06-26 12:51:13 +0200

    maj save settings & close modal on save

--- src/components/home/controllers/homeCtrl.js
+++ src/components/home/controllers/homeCtrl.js
@@ -31,12 +31,14 @@ function HomeCtrl($sce, $scope) {
 			urlAppStore : 'https://itunes.apple.com/fr/app/uber-votre-chauffeur-prive/id368677368?mt=8&v0=WWW-EUFR-ITSTOP100-FREEAPPS&l=fr&ign-mpt=uo%3D4',
 			urlPlayStore : 'https://play.google.com/store/apps/details?id=com.ubercab&hl=fr'
 		}];
-
+	
+	this.selectedDeviceTemp = this.devices[1];	
 	this.selectedDevice = this.devices[1];
 
 }
 
 HomeCtrl.prototype.getSource = function () {
+	this.selectedDevice = this.selectedDeviceTemp;
 	this.source = _$sce.trustAsResourceUrl(this.iframe.url);
 };
 
